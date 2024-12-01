b478cfefd3f6babb696d2291f2c99d188869abeb
AlirezaBohlool <dev.alirezabohlool@gmail.com>
2024-12-01 16:46:31 +0330

    feat: add transaction sell and buy

--- src/sections/home/transaction/transAction.tsx
+++ src/sections/home/transaction/transAction.tsx
@@ -2,7 +2,32 @@
 import React, { useState } from "react";
 import Buy from "./buy/buy";
 import Sell from "./sell/sell";
+import HalfCircle from "@/assets/icons/halfCircle";
+import BNB from "@/assets/icons/bnb";
 
+const currencies = [
+  {
+    name: "بایننس کوین",
+    symbol: "BNB",
+    price:"43537353", 
+    change: "1.37",
+    icon: <BNB />,
+  },
+  {
+    name: "شیبا",
+    symbol: "SHIB",
+    price: 200, 
+    change: "0.42",
+    icon: <BNB />,
+  },
+  {
+    name: "اتریوم",
+    symbol: "ETH",
+    price: "54327353",
+    change: "-0.75",
+    icon: <BNB />,
+  },
+];
 export default function TransAction() {
   const [isBuy, setIsBuy] = useState(true);
 
@@ -11,12 +36,14 @@ export default function TransAction() {
   };
 
   return (
-    <div className="w-full">
-      <div className='flex gap-4 bg-[#F6F6F6] py-5 pr-6 rounded-xl'>
+    <div className="w-full border border-2 rounded-xl">
+      <div className="flex gap-4 bg-gray-300 py-5 pr-6 rounded-xl cursor-pointer dark:bg-slate-700">
         <div onClick={toggleTransaction}>خرید از ارزهشت</div>
         <div onClick={toggleTransaction}>فروش به ارز هشت</div>
       </div>
-      <div>{isBuy ? <Buy /> : <Sell />}</div>
+      <div className="relative w-full bg-background h-[180px] px-5 ">
+         {isBuy?<Buy currencies={currencies} toggle={toggleTransaction} />:<Sell currencies={currencies} toggle={toggleTransaction}/>}
+      </div>
     </div>
   );
 }
