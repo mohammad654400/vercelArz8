ca8afc636bb6c8019a73064fd3a52600143b3d8a
AlirezaBohlool <dev.alirezabohlool@gmail.com>
2024-12-01 16:51:59 +0330

    feat : quick guid

---
+++ src/sections/home/quickguide/quickGuide.tsx
@@ -0,0 +1,138 @@
+'use client';
+import ArrowDown from "@/assets/icons/arrowDown";
+import ArrowLeft from "@/assets/icons/arrowLeft";
+import ArrowUp from "@/assets/icons/arrowup";
+import { useState } from "react";
+
+interface FAQItem {
+  question: string;
+  answer: string;
+  videoUrl?: string;
+}
+
+const faqs: FAQItem[] = [
+  {
+    question: "چگونه ارز دیجیتال بخریم؟",
+    answer: "با مراجعه به بخش خرید و فروش می‌توانید اقدام به خرید کنید.",
+    videoUrl: "https://www.aparat.com/v/tyuazi3",
+  },
+  {
+    question: "چگونه ارز دیجیتال بفروشیم؟",
+    answer: "پس از تایید کیف پول و ثبت‌نام، به بخش فروش مراجعه کنید و ارز دیجیتال خود را بفروشید.",
+  },
+  {
+    question: "چگونه ارز دیجیتال بفروشیم؟",
+    answer: "پس از تایید کیف پول و ثبت‌نام، به بخش فروش مراجعه کنید و ارز دیجیتال خود را بفروشید.",
+  },
+  {
+    question: "چگونه ارز دیجیتال بفروشیم؟",
+    answer: "پس از تایید کیف پول و ثبت‌نام، به بخش فروش مراجعه کنید و ارز دیجیتال خود را بفروشید.",
+  },
+  {
+    question: "چگونه ارز دیجیتال بفروشیم؟",
+    answer: "پس از تایید کیف پول و ثبت‌نام، به بخش فروش مراجعه کنید و ارز دیجیتال خود را بفروشید.",
+  },
+  {
+    question: "چگونه در صرافی ثبت‌نام کنیم؟",
+    answer: "مراحل ثبت‌نام را تکمیل کرده و اطلاعات خود را وارد کنید.",
+    videoUrl: "https://www.aparat.com/v/tyuazi3",
+  },
+  {
+    question: "چگونه در صرافی ثبت‌نام کنیم؟",
+    answer: "مراحل ثبت‌نام را تکمیل کرده و اطلاعات خود را وارد کنید.",
+    videoUrl: "https://www.aparat.com/v/tyuazi3",
+  },
+];
+
+const QuickGuide = () => {
+  const [activeRightIndex, setActiveRightIndex] = useState<number | null>(null);
+  const [activeLeftIndex, setActiveLeftIndex] = useState<number | null>(null);
+
+  const rightFAQs = faqs.filter((faq) => !faq.videoUrl);
+  const leftFAQs = faqs.filter((faq) => faq.videoUrl);
+
+  const toggleAccordion = (
+    index: number,
+    setActiveIndex: React.Dispatch<React.SetStateAction<number | null>>,
+    activeIndex: number | null
+  ) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const sanitizeApparatUrl = (url: string) => {
+    const videoId = new URL(url).pathname.split('/').pop();
+    return `https://www.aparat.com/video/video/embed/videohash/${videoId}/vt/frame`;
+  };
+
+  return (
+    <div className="w-full flex flex-col md:flex-row gap-4 mx-auto bg-background rounded-2xl">
+      <div className="w-full md:w-1/2 pr-4">
+        <h2 className="text-lg font-bold mb-8">راهنمای سریع</h2>
+        {rightFAQs.map((faq, index) => (
+          <div key={index} className="mb-2 border-2 rounded-2xl px-3">
+            <button
+              onClick={() =>
+                toggleAccordion(index, setActiveRightIndex, activeRightIndex)
+              }
+              className="w-full text-right flex justify-between items-center py-3  font-medium"
+            >
+              {faq.question}
+              <span className="transform transition-transform">
+                {activeRightIndex === index ? <ArrowUp /> : <ArrowDown />}
+              </span>
+            </button>
+            {activeRightIndex === index && (
+              <div className="px-4 py-2 ">
+                <p>{faq.answer}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full md:w-1/2">
+        <div className="flex justify-end mb-1">
+          <button className="flex gap-2 items-center px-6 bg-primary mb-4 text-background py-2 rounded-2xl">
+            همه سوالات <ArrowLeft />
+          </button>
+        </div>
+        <div className="p-5 border rounded-2xl">
+          <div className="flex justify-center py-4 text-lg ">
+            فیلم‌های آموزشی
+          </div>
+          {leftFAQs.map((faq, index) => (
+            <div key={index} className="mb-2 border-2 rounded-2xl px-3">
+              <button
+                onClick={() =>
+                  toggleAccordion(index, setActiveLeftIndex, activeLeftIndex)
+                }
+                className="w-full text-right flex justify-between items-center py-3  font-medium"
+              >
+                {faq.question}
+                <span className="transform transition-transform">
+                  {activeLeftIndex === index ? <span className="dark:text-white"><ArrowUp /></span> : <ArrowDown />}
+                </span>
+              </button>
+              {activeLeftIndex === index && faq.videoUrl && (
+                <div className="px-4 py-2 ">
+                  <p>{faq.answer}</p>
+                  <div className="mt-4">
+                    <iframe
+                      src={sanitizeApparatUrl(faq.videoUrl)}
+                      title="آموزش ویدئویی"
+                      className="w-full aspect-video rounded-md shadow-md"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuickGuide;
