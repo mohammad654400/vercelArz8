57f238a84491ee5d3a4175cb82feb7dcae5871b2
lazyone.m <lazyone.m@gmail.com>
2025-01-19 15:02:34 +0330

    feat close modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,18 +22,21 @@ const Modal: React.FC<ModalProps> = ({ isOpen, type, icon, lines, onClose }) => 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div 
+    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+    onClick={onClose}
+    >
       <div
         className="relative bg-white dark:bg-[#242428] p-6  lg:w-[513.19px] lg:h-[481px]  w-[287px] h-[269.06px]
        border-[1.28px] border-[#FFC107] rounded-[35px] lg:rounded-[50px]  border-solid shadow-lg"
       >
        
-        <button
+        {/* <button
           className="absolute top-6 right-6  hover:text-gray-700"
           onClick={onClose}
         >
           ✕
-        </button>
+        </button> */}
 
       
         <div className="flex justify-center items-center mt-10 mb-5 lg:mt-[70px] lg:mb-9">{icon}</div>
