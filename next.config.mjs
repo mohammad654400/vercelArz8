46a5c1345a99241ef2e0406763e7f8f4cef3cb2e
AlirezaBohlool <dev.alirezabohlool@gmail.com>
2025-02-08 14:52:57 +0330

    feat : add PWA feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,38 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["app.arz8.com"],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'app.arz8.com'
+    }]
   },
-    async rewrites() {
-      return [
-        {
-          source: "/api/proxy/landing/info", 
-          destination: "https://app.arz8.com/api/landing/info", 
-        },
-        {
-          source: "/api/proxy/landing/home", 
-          destination: "https://app.arz8.com/api/landing/home", 
-        },
-        {
-          source: "/api/proxy/landing/cryptocurrencies", 
-          destination: "https://app.arz8.com/api/landing/cryptocurrencies", 
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy/landing/info",
+        destination: "https://app.arz8.com/api/landing/info",
+      },
+      {
+        source: "/api/proxy/landing/home",
+        destination: "https://app.arz8.com/api/landing/home",
+      },
+      {
+        source: "/api/proxy/landing/cryptocurrencies",
+        destination: "https://app.arz8.com/api/landing/cryptocurrencies",
+      },
+    ];
+  },
+};
+
+// Create the PWA configuration wrapper
+const withPWAConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+// Apply the PWA configuration to the Next.js config
+export default withPWAConfig(nextConfig);
