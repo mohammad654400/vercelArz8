9d7be56f025942f437b04520c0f1280c0cf5c7f0
AlirezaBohlool <dev.alirezabohlool@gmail.com>
2025-03-03 09:08:47 +0330

    fix : next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-{
-  "headers": [
+export default {
+  headers: [
     {
-      "source": "/(.*)",
-      "headers": [
-        { "key": "Access-Control-Allow-Origin", "value": "*" },
-        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,POST" },
-        { "key": "Access-Control-Allow-Headers", "value": "Content-Type, Authorization, X-Timestamp, X-Signature" }
+      source: "/(.*)",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST" },
+        { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Timestamp, X-Signature" }
       ]
     }
   ]
-}
+};
